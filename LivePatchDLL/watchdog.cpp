973742841e40d60b0aa9b5975c4a0eec78c35812
Yakov5776 <yakov5776@gmail.com>
2022-04-11 11:41:14 -0400

    Add LoadCount

--- LivePatchDLL/watchdog.cpp
+++ LivePatchDLL/watchdog.cpp
@@ -8,15 +8,23 @@ typedef struct IUnknown IUnknown;
 #include <chrono>
 #include <iostream>
 #include <Windows.h>
+#include <mutex>
+
+volatile int LoadCount = 0;
 
 void watchdog::StartThreadIntegrity() {
+    int TotalLoadCount = 0;
     // Run ProtectionModules on new thread.
 	
     typedef void (*func)();
     func arr[]{NoDump::RunEngine, NoDump::RunEngine};
     for (auto* x : arr) {
+        TotalLoadCount++;
+        _InterlockedIncrement((long volatile*)&LoadCount);
         NEWTHREAD(x);
     }
+
+	std::cout << "Loaded " << LoadCount << "/" << TotalLoadCount << " Modules" << std::endl;
 }
 
 void watchdog::HandleException() {
@@ -24,12 +32,15 @@ void watchdog::HandleException() {
         throw;
     }
     catch (const std::runtime_error& e) {
+        _InterlockedDecrement((long volatile*)&LoadCount);
         std::cout << ERRORMSG << "Runtime Exception: " << e.what() << std::endl;
     }
     catch (const std::exception& e) {
+        _InterlockedDecrement((long volatile*)&LoadCount);
         std::cout << ERRORMSG << "Exception: " << e.what() << std::endl;
     }
     catch (...) {
+        _InterlockedDecrement((long volatile*)&LoadCount);
         std::cout << ERRORMSG << "Unknown Exception!" << std::endl;
     }
 }
