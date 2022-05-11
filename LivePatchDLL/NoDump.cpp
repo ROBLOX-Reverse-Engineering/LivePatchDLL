1f6acda53d47368af7b70fe53275d6ff5ce1e922
Yakov5776 <yakov5776@gmail.com>
2022-05-11 18:53:38 -0400

    Use define markers for start and end of protection modules

--- LivePatchDLL/NoDump.cpp
+++ LivePatchDLL/NoDump.cpp
@@ -1,16 +1,13 @@
+#define EnabledModule true
 #include <iostream>
 #include "NoDump.h"
 #include "watchdog.h"
 
 void NoDump::RunEngine() {
 	const char* ModuleName = "NoDump";
-
-	try {
-		std::cout << "[" << ModuleName << "] Module has loaded!" << std::endl;
-	}
-	catch (...) {
-		watchdog::HandleException(ModuleName, __FUNCTION__);
-	}
+	START_MODULE;
+	
+	END_MODULE;
 }
 
 // create event when handle creation, loop through child-windows and if it contains process name, code red.
