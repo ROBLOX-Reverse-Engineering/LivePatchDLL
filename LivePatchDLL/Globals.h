cb0d2eb594a38f6e75affac1f111cef72226b541
Brent Da Mage <BrentDaMage@users.noreply.github.com>
2022-12-08 19:53:05 -0600

    Moved address macros from Globals.h to Addresses.h

--- LivePatchDLL/Globals.h
+++ LivePatchDLL/Globals.h
@@ -2,16 +2,6 @@
 
 #define APP_VERSION 132
 
-// Version-Based Address
-#define VERSION_ADDR(ver, addr) ver == APP_VERSION ? addr : 
-// Defines an address with the given name and value
-#define ADDR(name, contents) static const unsigned int name = contents;
-// Captures a set of arguments (for ignoring commas)
-#define ARGS(...) __VA_ARGS__
-// Defines a function using the given return type, name, arguments, and address contents
-#define FUNC(type, name, args, contents) typedef type(* name ## _ptr)(args); \
-const name ## _ptr name = ( name ## _ptr)(contents);
-
 #if APP_VERSION <= 146
 	#define VS2008
 #endif
--- LivePatchDLL/include/Addresses.h
+++ LivePatchDLL/include/Addresses.h
@@ -1,5 +1,15 @@
 #pragma once
 
+// Version-Based Address
+#define VERSION_ADDR(ver, addr) ver == APP_VERSION ? addr : 
+// Defines an address with the given name and value
+#define ADDR(name, contents) static const unsigned int name = contents;
+// Captures a set of arguments (for ignoring commas)
+#define ARGS(...) __VA_ARGS__
+// Defines a function using the given return type, name, arguments, and address contents
+#define FUNC(type, name, args, contents) typedef type(* name ## _ptr)(args); \
+const name ## _ptr name = ( name ## _ptr)(contents);
+
 class SavedAddr
 {
 public:
